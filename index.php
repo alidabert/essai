ada988f5128a60cf9f38e2d5982b8cf37106e542
user.name <beralie@yahoo.fr>
2015-11-06 08:48:08 +0100

    "ajout du fichier index"

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.min.css" />
+<script type="text/javascript" src="bootstrap-3.3.5-dist/js/jquery-1.11.3.js"></script>
+<script type="text/javascript" src="bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+<title>Document sans titre</title>
+</head>
+
+<body>
+		<div class="well">
+			<strong style="font-size:18px">ZANTERBENG VILLAGE</strong>
+			<a class="pull-right"> Connexion</a>&nbsp;&nbsp;
+			<a class="pull-right" style="padding-right:50px"> Inscription</a>
+		</div>                                  
+                                                                                                                                                                                                                                                                
+																																													        <div id="myCarousel" class="carousel slide" style="margin-top:-20px;"> <!-- Carousel indicators --> <ol class="carousel-indicators"> <li data-target="#myCarousel" data-slide-to="0" class="active"></li> <li data-target="#myCarousel" data-slide-to="1"></li> <li data-target="#myCarousel" data-slide-to="2"></li> </ol> <!-- Carousel items --> <div class="carousel-inner"> <div class="item active"> <img src="image/header (7).jpg" alt="First slide"> </div> <div class="item"> <img src="image/header (8).jpg" alt="Second slide"> </div> <div class="item"> <img src="image/header (9).jpg" alt="Third slide"> </div> </div> <!-- Carousel nav --> <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a> <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+																																																																																												        </div> 
+		
+		
+		<nav class="navbar navbar-default" role="navigation"> <div class="navbar-header"> <a class="navbar-brand" href="#">Accueil</a> </div> <div id="myexample"> <ul class="nav navbar-nav"> <li class="active"><a href="#">Historique</a></li> <li><a href="#">Activités</a></li><li><a href="#">Espace</a></li><li><a href="#">Forum</a></li><li><a href="#">Contact</a></li> <li class="dropdown"> <a href="#" class="dropdown-toggle">Espace77<b class="caret"></b></a> <ul class="dropdown-menu"> <li><a id="action-1" href="#"> jmeter</a> </li> <li><a href="#">EJB</a></li> <li><a href="#">Jasper Report</a></li> <li class="divider"></li> <li><a href="#">Separated link</a></li> <li class="divider"></li> <li><a href="#">One more separated link</a></li> </ul> </li> </ul> </div> </nav></div>	
+		
+	   <div class="container" style="background-color:#00FFFF; margin-top:-20px">jhjjjfgf
+	   <img class="img-responsive" src="image/Baguette 02.png" />
+	   </div>																																											
+</body>
+
+<script>
+$('#myCarousel').carousel({ interval: 2000});
+</script>
+
+</html>
